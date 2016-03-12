4cc1f2fae6c5ae7e443edbc2eae03e33d4a197d8
Panjunlin <GEEK@panjunlindeMBP.lan>
2016-03-12 21:39:58 +0800

    add Decorator

---
+++ src/Decorator/DarkRoast.java
@@ -0,0 +1,11 @@
+package Decorator;
+
+public class DarkRoast extends Beverage{
+		 DarkRoast(){
+		  this.description	 =	"DarkRoast coffee";
+		}
+		
+		public double cost(){
+			return .99;
+		}
+}
